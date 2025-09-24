515f83dfc8b56dded150382431d56adb0570986c
tapash22 <tapasp263@gmail.com>
2025-09-24 10:19:24 +0600

    make todo app

---
+++ src/main/java/com/example/todospring/TodospringApplication.java
@@ -0,0 +1,13 @@
+package com.example.todospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodospringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodospringApplication.class, args);
+	}
+
+}
